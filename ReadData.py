42cd1edd827b871c0e4b54f646eb192bd843e0c8
calvinji <calvinchenji@gmail.com>
2023-03-29 14:49:31 -0400

    county_size_function

--- ReadData.py
+++ ReadData.py
@@ -1,5 +1,5 @@
 import datetime
-
+from math import pi #This is for the estimate size function - Calvin
 
 def read_csv(file_name: str) -> tuple[float, str, str, float]:
     """ Reads the csv file and returns a tuple of (time, start_loc, stop_loc, distance)
@@ -87,6 +87,34 @@ def get_avg_times_and_miles(l: list[tuple[datetime.time, str, str, float]]) -> d
         stop = listy[1]
         final_dict[{start, stop}] += [avg_miles]
     return final_dict
+
+
+def estimate_county_size(county_name: str, data: dict[set[str]:list[float]]) -> tuple[float]:
+    """Make a rough estimate of of the county size in miles squared and return it, where 
+    the first float in the tuple is a lower bound and the second float in the tuple is a upper bound.
+    We can assume that the county is a perfect sphere and assume that on average that the start
+    location is in the middle of the county. So we get the average length of rides that start and 
+    stop in the same city, and that is our lower bound for our radius. So lower bound of the area
+    is the average length of rides that start and stop in the same city squared times pi (we could 
+    approximate pi as 3.14 or use math.pi) Then for upper bound we could find the average distance 
+    from the city whose area we are trying to approximate to every other city that it is connected to. 
+    We take the smallest average distance because presumably that would be the closest city that is 
+    nearby, and that could be a upper bound for our radius."""
+    lower_radius = 0
+    for set_of_county_names in data:
+        if set_of_county_names == {county_name, county_name}:
+            lower_radius = data[set_of_county_names][1]
+    lower_bound = pi * (lower_radius) ** 2
+
+    avg_distances_to_cities = [0]
+    for set_of_county_names in data:
+        if set_of_county_names.contains(county_name) and set_of_county_names != {county_name, county_name}:
+            avg_distances_to_cities.append(data[set_of_county_names][1])
+    upper_radius = min(avg_distances_to_cities)
+    upper_bound = pi * (upper_radius) ** 2
+
+    return (lower_bound, upper_bound)
+
 
 
 if __name__ == '__main__':
